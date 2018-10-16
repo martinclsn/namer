f583e8cab8e0ba5e5868c93aaa636ee8a3b5142d
martin claesson <martin@clsn.se>
2018-10-16 23:32:59 +0200

    update

---
+++ src/main/java/Main.java
@@ -0,0 +1,35 @@
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        List<String> part1List = Resources.readLines(new File("part1.txt").toURI().toURL(), Charsets.UTF_8);
+        List<String> part2List = Resources.readLines(new File("part2.txt").toURI().toURL(), Charsets.UTF_8);
+
+        for (int i = 0; i < 200; i++) {
+            String a = getRandom(part1List, "");
+            String b = getRandom(part2List, "");
+            log(a + " " + b);
+        }
+
+    }
+
+    private static String getRandom(List<String> names, String excludeName) {
+        int pos = new Random().nextInt(names.size());
+        String result = names.get(pos).trim();
+        if(result.length() == 0 || excludeName.equals(result)) {
+            return getRandom(names, excludeName);
+        }
+        return result;
+    }
+
+    private static void log(String string) {
+        System.out.println(string);
+    }
+}
